8abe642f89811def34ba4ecf513b4438f374c5d5
bordato87 <bordato87@gmail.com>
2021-01-28 15:12:05 -0300

    homework V finalizada

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -65,7 +65,7 @@ function agregarStringInvertida() {
       this.edad = edad;
       this.domicilio = domicilio;
     }
-    detalle = function() { 
+    detalle() { 
        return{
         Nombre: this.nombre,
         Apellido: this.apellido,
@@ -79,7 +79,7 @@ function crearInstanciaPersona(nombre, apellido, edad, dir) {
   //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
   //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
   //Devolver la nueva persona creada
-  var persona = new Persona("Juan", "Perez", 22, "Saavedra 123")
+  const persona = new Persona("Juan", "Perez", 22, "Saavedra 123")
   return persona;
 }
   
